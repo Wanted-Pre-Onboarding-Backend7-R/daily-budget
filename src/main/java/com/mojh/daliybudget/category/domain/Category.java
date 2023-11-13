19ce1edf78a4efb99a558c42298a140a46bbf16c
mojh7 <mjh79017901@gmail.com>
2023-11-13 12:57:52 +0900

    feat: 카테고리 정의

--- src/main/java/com/mojh/daliybudget/category/domain/Category.java
+++ src/main/java/com/mojh/daliybudget/category/domain/Category.java
@@ -7,13 +7,15 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Getter
-@EqualsAndHashCode(of = "name", callSuper = false)
+@EqualsAndHashCode(of = "type", callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
 
@@ -21,11 +23,12 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 40)
-    private String name;
+    @Column(nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
+    private CategoryType type = CategoryType.UNCATEGORIZED;
 
-    public Category(String name) {
-        this.name = name;
+    public Category(CategoryType type) {
+        this.type = type;
     }
 
 }
