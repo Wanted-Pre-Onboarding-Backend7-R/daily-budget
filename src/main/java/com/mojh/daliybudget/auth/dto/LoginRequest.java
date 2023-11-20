0b141ab94aed5494ecd2250d17a2d53b1b82d535
mojh7 <mjh79017901@gmail.com>
2023-11-20 22:16:43 +0900

    feat: 회원 로그인, 로그아웃, 토큰 재발급 구현
     - 토큰 재발급 refresh token rotation 적용

---
+++ src/main/java/com/mojh/daliybudget/auth/dto/LoginRequest.java
@@ -0,0 +1,27 @@
+package com.mojh.daliybudget.auth.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginRequest {
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String accountId;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String password;
+
+    public LoginRequest(String accountId, String password) {
+        this.accountId = accountId;
+        this.password = password;
+    }
+
+}
