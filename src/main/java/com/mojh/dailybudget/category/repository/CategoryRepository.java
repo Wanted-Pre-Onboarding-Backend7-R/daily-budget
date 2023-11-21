af263df8218cc8b67fd87ec109fbd61e07e5bdb4
mojh7 <mjh79017901@gmail.com>
2023-11-22 03:18:24 +0900

    feat: CategoryType으로 카테고리 조회 추가

--- src/main/java/com/mojh/dailybudget/category/repository/CategoryRepository.java
+++ src/main/java/com/mojh/dailybudget/category/repository/CategoryRepository.java
@@ -1,8 +1,13 @@
 package com.mojh.dailybudget.category.repository;
 
 import com.mojh.dailybudget.category.domain.Category;
+import com.mojh.dailybudget.category.domain.CategoryType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByType(CategoryType type);
 
 }
