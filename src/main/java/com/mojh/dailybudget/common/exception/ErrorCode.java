0656cfbc5e34e3a201453cd79bf4040e48eab180
mojh7 <mjh79017901@gmail.com>
2023-11-23 02:12:01 +0900

    feat: ErrorCode 필드 추가

--- src/main/java/com/mojh/dailybudget/common/exception/ErrorCode.java
+++ src/main/java/com/mojh/dailybudget/common/exception/ErrorCode.java
@@ -18,6 +18,7 @@ public enum ErrorCode {
     COM_INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, "COM0000", "시스템 오류입니다."),
     COM_BAD_REQUEST(BAD_REQUEST, "COM0001", "잘못된 요청입니다."),
     COM_INVALID_PARAMETERS(BAD_REQUEST, "COM0002", "유효하지 않은 파라미터입니다."),
+    COM_PATCH_FAILED(BAD_REQUEST, "COM0003", "데이터 수정에 실패했습니다."),
 
     // member
     MEMBER_NOT_FOUND(NOT_FOUND, "MEM0001", "유저를 찾을 수 없습니다."),
