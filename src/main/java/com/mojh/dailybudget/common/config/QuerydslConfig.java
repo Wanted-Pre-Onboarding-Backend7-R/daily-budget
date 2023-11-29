b50b38d68f2079fca6692e5e3142e5a738f33254
mojh7 <mjh79017901@gmail.com>
2023-11-29 13:54:09 +0900

    setup: querydsl 의존성 및 QuerydslConfig 추가

---
+++ src/main/java/com/mojh/dailybudget/common/config/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package com.mojh.dailybudget.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+
+@Configuration
+public class QuerydslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
